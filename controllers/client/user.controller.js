939fa44bacb2d1cb46896a6ab7eb5a823230d3d5
minductran <minhductran042@gmail.com>
2024-09-21 11:03:13 +0700

    Tinh nang dang nhap

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,6 +1,7 @@
 const User = require("../../models/user.model");
 const md5 = require('md5');
 const generateHelper = require("../../helpers/generate.hepler");
+
 // [POST] /users/register
 module.exports.register = async (req,res) => {
     try {   
@@ -17,7 +18,7 @@ module.exports.register = async (req,res) => {
             return;
         }
 
-        
+
         const token = generateHelper.generateRandomString(30);
         const userDataRegister = {
             fullName: req.body.fullName,
@@ -26,7 +27,9 @@ module.exports.register = async (req,res) => {
             token: token
         }
         
-        console.log(userDataRegister);
+        const user = new User(userDataRegister);
+        await user.save();
+        
         res.json({
             code: 200,
             message: "Đăng ký thành công!"
@@ -36,4 +39,40 @@ module.exports.register = async (req,res) => {
             message: "Not Found"
         });
     }
+}
+
+
+// [POST] /users/login
+module.exports.login = async (req,res) => {
+    
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if(!user) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại"
+        })
+        return;
+    }
+
+    if(md5(password) != user.password) {
+        res.json({
+            code: 400,
+            message: "Sai mật khẩu"
+        })
+        return;
+    }
+
+    
+    res.json({
+        code: 200,
+        message: "Đăng nhập thành công",
+        token: user.token
+    })
 }
